96f71b656bc002b14cc6235fe516ab0df3d08032
Rafael Farias <rpf1rafael@gmail.com>
2026-05-04 22:36:46 -0300

    Projeto inicial: Spring Boot CRM para estética automotiva

---
+++ src/main/java/com/garagem77/Garagem77Application.java
@@ -0,0 +1,13 @@
+package com.garagem77;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Garagem77Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Garagem77Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/garagem77/Garagem77ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garagem77;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Garagem77ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
